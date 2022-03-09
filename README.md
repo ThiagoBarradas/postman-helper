d8b15e85ba2670ccae8a37acc19be5fcc8fea669
Thiago Barradas <th.barradas@gmail.com>
2022-03-09 04:51:17 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Postman External Require
+
+Use Node packages not bundled into Postman's sandbox.
+
+## 1. Setup on local machine
+
+Clone this repo.
+
+`npm i`
+
+`node index.js`
+
+## 2. Setup in Postman
+
+If you want to jump right into it, import this prebuilt collection and create a global variable called `require` with value `uniq,slapdash,pad-left`:
+
+Read on for getting this working with your own existing collection..
+
+Add the following script to the `Pre-Request Script` of your collection:
+
+```js
+const reqs = pm.globals.get('require');
+const pkgs = pm.globals.get('packages');
+const installed = pm.globals.get('installedPackages');
+
+if (pkgs && (reqs === installed)) {
+    eval(pkgs)
+} else {
+    pm.sendRequest({
+        url: `localhost:3000/search-packages?packages=${reqs}`,
+        method: 'GET'
+    }, (err, res) => {
+        if (!err) {
+            eval(res.text());
+            pm.globals.set('installedPackages', reqs);
+            pm.globals.set('packages', res.text());
+        }
+    });
+}
+```
+
+Create a global variable in Postman called `require`. Provide a comma separated listed of packages you need e.g. `uniq,slapdash,pad-left`.
+
+`require` your packages in the usual way from within folder/request scripts!
+
+## 3. Login as User (Sample for Stone Banking)
+
+`POST http://localhost:3000/user-login`
+
+```
+{
+    "username" : "{{Document}}",
+    "password" : "{{Password}}",
+    "url": "{{BaseUrl}}/api/v1/discovery/keys",
+    "scope": "stone_subject_id",
+    "client_id": "homebanking@openbank.stone.com.br"
+}
+```
+
---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const jose = require('node-jose');
+const axios = require('axios');
+const app = express();
+const childProcess = require('child_process');
+
+app.use(express.json());
+
+app.get('/search-packages', (request, response) => 
+{
+  try 
+  {
+    var packages = request.query.packages.split(',');
+    var browserify = require('browserify')();
+
+    for (let i = 0; i < packages.length; i++) {
+      var pkg = packages[i];
+
+      childProcess.execSync(`npm install ${pkg}`);
+      browserify.require(pkg);
+    }
+
+    browserify.bundle((error, buffer) => {
+      if (error) { 
+        console.log(error)
+      }
+      
+      response.send(buffer.toString());
+    });
+  } 
+  catch (exception) {
+    response.send('Something went wrong!');
+  }
+});
+
+app.post('/user-login', (request, response) => {
+  try 
+  {
+    var user = request.body.username;
+    var pass = request.body.password;
+    var client_id = request.body.client_id;
+    var scope = request.body.scope;
+    var url = request.body.url;
+
+    axios.get(url)
+    .then(keys => {
+      var payload = {
+        client_id: client_id,
+        scope: scope,
+        username: user,
+        password: pass,
+        request_metadata: {}
+      };
+
+      var contentToEncrypt = JSON.stringify(payload);    
+      var buffer = Buffer.from(contentToEncrypt)
+
+      //changing alg/enc
+      keys.data.keys.forEach(function(item){
+        if (item.use=="enc") {
+          item.alg = "RSA-OAEP-256";
+        }
+      });
+
+      jose.JWK.asKeyStore(keys.data).
+      then(function(keystore) {
+        var key = keystore.all({ use: 'enc' });
+        jose.JWE.createEncrypt({ format: 'compact', contentAlg: "A256GCM" }, key)
+          .update(buffer)
+          .final()
+          .then((encryptedContent) => {  
+            var data = { jwe: encryptedContent };
+            response.json(data);
+          });       
+      });
+    });
+  } 
+  catch (exception) {
+    response.send('Something went wrong!')
+  }
+});
+
+app.listen(3000, () => console.log(`Running!`))
